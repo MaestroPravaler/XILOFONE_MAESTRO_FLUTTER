4cc1dcbac343da9af516faaf82247e821cfea242
RobsonMaestro <robson_maestro@alumni.usp.br>
2021-02-13 20:33:59 -0300

    Commit Inicial do Projeto

---
+++ android/app/src/main/kotlin/br/com/robsonmaestro/xilofone_maestro/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.robsonmaestro.xilofone_maestro
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/modules/splashscreen/controllers/splashscreen_controller.dart
@@ -0,0 +1,18 @@
+import 'package:get/get.dart';
+
+class SplashscreenController extends GetxController {
+  //TODO: Implement SplashscreenController
+
+  @override
+  void onInit() {
+    super.onInit();
+  }
+
+  @override
+  void onReady() {
+    super.onReady();
+  }
+
+  @override
+  void onClose() {}
+}
