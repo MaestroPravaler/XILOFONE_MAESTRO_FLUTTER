a5bef6485ec6118e7953ce927dd196c93e35d275
RobsonMaestro <robson_maestro@alumni.usp.br>
2021-04-25 17:00:43 -0300

    Migração para a Versão Null Safety

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -4,26 +4,19 @@ import 'package:xilofone_maestro/app/modules/home/bindings/home_binding.dart';
 import 'package:xilofone_maestro/app/modules/home/views/home_view.dart';
 import 'package:xilofone_maestro/app/modules/sobre/bindings/sobre_binding.dart';
 import 'package:xilofone_maestro/app/modules/sobre/views/sobre_view.dart';
-import 'package:xilofone_maestro/app/modules/splashscreen/bindings/splashscreen_binding.dart';
-import 'package:xilofone_maestro/app/modules/splashscreen/views/splashscreen_view.dart';
 import 'package:xilofone_maestro/app/modules/treino/bindings/treino_binding.dart';
 import 'package:xilofone_maestro/app/modules/treino/views/treino_view.dart';
 
 part 'app_routes.dart';
 
 class AppPages {
-  static const INITIAL = Routes.SPLASHSCREEN;
+  static const INITIAL = Routes.HOME;
 
   static final routes = [
     GetPage(
       name: _Paths.HOME,
       page: () => HomeView(),
       binding: HomeBinding(),
-    ),
-    GetPage(
-      name: _Paths.SPLASHSCREEN,
-      page: () => SplashscreenView(),
-      binding: SplashscreenBinding(),
     ),
     GetPage(
       name: _Paths.SOBRE,
